80f296253ace9d0fbfd261027f4049c7508a0a55
Aldo Flores <sarduts@gmail.com>
2025-04-07 22:06:44 -0600

    crea la clase Serie

---
+++ src/com/aluracursos/screenmatch/modelos/Serie.java
@@ -0,0 +1,31 @@
+package com.aluracursos.screenmatch.modelos;
+
+public class Serie extends Titulo{
+    private int temporada;
+    private int episodiosPorTemporada;
+    private int minutosPorEpisodio;
+
+    public int getTemporada() {
+        return temporada;
+    }
+
+    public int getEpisodiosPorTemporada() {
+        return episodiosPorTemporada;
+    }
+
+    public int getMinutosPorEpisodio() {
+        return minutosPorEpisodio;
+    }
+
+    public void setTemporada(int temporada) {
+        this.temporada = temporada;
+    }
+
+    public void setEpisodiosPorTemporada(int episodiosPorTemporada) {
+        this.episodiosPorTemporada = episodiosPorTemporada;
+    }
+
+    public void setMinutosPorEpisodio(int minutosPorEpisodio) {
+        this.minutosPorEpisodio = minutosPorEpisodio;
+    }
+}
