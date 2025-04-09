65674b058851b7c25a13e4a8991e7301e9a2f12e
Aldo Flores <sarduts@gmail.com>
2025-04-09 11:05:35 -0600

    crea la clase FiltroRecomendacion

---
+++ src/com/aluracursos/screenmatch/calculos/FiltroRecomendacion.java
@@ -0,0 +1,13 @@
+package com.aluracursos.screenmatch.calculos;
+
+public class FiltroRecomendacion {
+    void filtra(Clasificacion clasificacion){
+        if(clasificacion.getClasificacion() >= 4){
+            System.out.println("Muy bien evaluado en el momento");
+        }else if(clasificacion.getClasificacion() >= 2){
+            System.out.println("Popular en el moemnto");
+        }else{
+            System.out.println("Colócalo en tu lista para verlo despues");
+        }
+    }
+}
